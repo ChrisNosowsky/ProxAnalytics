79c256088fb7cc623a789499038f1030e3bd0157
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-06-06 00:20:11 -0400

    new changes to landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-root',
@@ -7,35 +7,10 @@ import { HttpClient, HttpClientModule } from '@angular/common/http'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  serverData: JSON 
-  employeeData: JSON
-  employee: JSON
-
-  constructor(private httpClient: HttpClient) {
+  constructor() {
 
   }
 
   ngOnInit() {}
-
-  sayHi() {
-    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
-      this.serverData = data as JSON
-      console.log(this.serverData)
-    })
-  }
-
-  getAllEmployees() {
-    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
-      this.employeeData = data as JSON
-      console.log(this.employeeData)
-    })
-  }
-
-  getEmployee() {
-    this.httpClient.get('http://127.0.0.1:5002/employees/1').subscribe(data => {
-      this.employee = data as JSON
-      console.log(this.employee)
-    })
-  }
 
 }
--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.component.html',
   styleUrls: ['./splash.component.scss']
 })
-export class SplashComponent implements OnInit {
 
-  constructor() { }
+export class SplashComponent implements OnInit {
+  serverData: JSON 
+  employeeData: JSON
+  employee: JSON
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON
+      console.log(this.serverData)
+    })
+  }
+
+  getAllEmployees() {
+    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
+      this.employeeData = data as JSON
+      console.log(this.employeeData)
+    })
+  }
+
+  getEmployee() {
+    this.httpClient.get('http://127.0.0.1:5002/employees/1').subscribe(data => {
+      this.employee = data as JSON
+      console.log(this.employee)
+    })
   }
 
 }
