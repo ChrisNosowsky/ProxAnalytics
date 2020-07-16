7eaa521adbfa19916160b3869c5976e22667e9f3
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-07-16 01:16:04 -0400

    Static charts, more cards, styling added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,4 +12,69 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public chartType: string = 'bar';
+
+  public chartDatasets: Array<any> = [
+    { data: [5, 10, 22, 4], label: '' }
+  ];
+
+  public chartLabels: Array<any> = ['Wins', 'Top 5', 'Top 10', 'DNF'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(255, 99, 132, 1)',
+      ],
+      borderWidth: 1,
+    }
+  ];
+
+  public chartOptions: any = {
+    responsive: true,
+    legend: {
+      display: false,
+    }
+  };
+
+  public chartLineType: string = 'line';
+
+  public chartLineDatasets: Array<any> = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+  ];
+
+  public chartLineLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+  public chartLineColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    },
+    {
+      backgroundColor: 'rgba(0, 137, 132, .2)',
+      borderColor: 'rgba(0, 10, 130, .7)',
+      borderWidth: 2,
+    }
+  ];
+
+  public chartLineOptions: any = {
+    responsive: true
+  };
+
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
+  public chartLineClicked(e: any): void { }
+  public chartLineHovered(e: any): void { }
+
 }
